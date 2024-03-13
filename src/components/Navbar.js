55f90ee7e3ca9fd3a4ccd1773968e8da4dddd65d
yash jain <yash@mediaamp.co.in>
2024-03-13 12:21:04 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import {
+  Navbar,
+  Nav,
+  Button,
+  NavDropdown,
+  Form,
+  Container,
+} from "react-bootstrap";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Header() {
+  const textColor = { color: "#DDDDDD" };
+
+  return (
+    <Navbar expand="lg" style={{ backgroundColor: "#12191D" }}>
+      <Container fluid>
+        {/* Dropdown */}
+        <Nav className="ml-auto">
+          <Button variant="" size="lg">
+            <FontAwesomeIcon icon={faBars} style={textColor} />
+          </Button>
+        </Nav>
+
+        <Navbar.Brand href="#" style={textColor}>
+          TimeChronos
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
