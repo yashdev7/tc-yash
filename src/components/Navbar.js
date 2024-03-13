b78cb6fa1199107ec1ff1bdc6abf25efbf768bd0
yash jain <yash@mediaamp.co.in>
2024-03-13 13:20:56 +0530

    Navbar 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,49 +15,23 @@ function Header() {
   return (
     <Navbar expand="lg" style={{ backgroundColor: "#12191D" }}>
       <Container fluid>
-        {/* Dropdown */}
-        <Nav className="ml-auto">
-          <Button variant="" size="lg">
-            <FontAwesomeIcon icon={faBars} style={textColor} />
-          </Button>
-        </Nav>
-
-        <Navbar.Brand href="#" style={textColor}>
-          TimeChronos
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+        <div className="d-flex center">
+          {/* Dropdown */}
+          <Nav className="ml-auto">
+            <Button variant="" size="lg">
+              <FontAwesomeIcon icon={faBars} style={textColor} />
+            </Button>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
+
+          {/* Navbar brand (logo) */}
+          <Navbar.Brand href="/">
+            <img
+              src="/timesheets_logo.png"
+              style={{ width: "100%", maxWidth: "120px" }}
+              alt=""
             />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+          </Navbar.Brand>
+        </div>
       </Container>
     </Navbar>
   );
